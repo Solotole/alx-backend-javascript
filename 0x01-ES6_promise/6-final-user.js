1f1c9bc2c51b4808dd0ebd9fde048700834082e2
Solotole <solomontoleak47@gmail.com>
2024-08-06 19:49:58 +0100

    Promise.all-Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,13 @@ import signUpUser from './4-user-promise';
 export default function handleProfileSignup(firstName, secondName, fileName) {
   return Promise.all([signUpUser(), uploadPhoto()])
     .then((result) => {
+      const [user, photo] = result;
+      photo.body = fileName;
+      user.firstName = firstName;
+      user.lastName = secondName;
       console.log(result);
+    })
+    .catch((err) => {
+      console.error(err);
     });
 }
