bd3c2db3529977ec26d2bd78565b77c3ad67e519
Solotole <solomontoleak47@gmail.com>
2024-08-07 11:35:52 +0100

    Promise.allSettled-staus and value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,11 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-export default function handleProfileSignup(firstName, secondName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, secondName), uploadPhoto()])
-    .then((result) => {
-      let [sign, upload] = result;
-      upload.body = fileName;
-      return result;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => (
+      result.status === 'fulfilled'
+        ? { status: 200, value: result.value }
+        : { status: 'rejected', value: result.reason }
+    )));
 }
