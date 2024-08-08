d47dae88f3077549cec8b988a55da24260279766
Solotole <solomontoleak47@gmail.com>
2024-08-08 10:29:01 +0100

    array of class objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const var1 = new ClassRoom(19);
+  const var2 = new ClassRoom(20);
+  const var3 = new ClassRoom(34);
+  return [var1, var2, var3];
+}
