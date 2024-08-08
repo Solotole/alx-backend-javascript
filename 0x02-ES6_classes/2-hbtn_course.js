aaa2269da473ef99373c49e689fa58d4bf0affbd
Solotole <solomontoleak47@gmail.com>
2024-08-08 10:30:19 +0100

    getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor (name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    for (const value in students) {
+      if (typeof students[value] !== 'string') {
+        throw new TypeError('Array contents must be strings');
+      }
+    }
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    for (const value in students) {
+      if (typeof students[value] !== 'string') {
+        throw new TypeError('Array contents must be strings');
+      }
+    }
+    this._students = students;
+  }
+}
