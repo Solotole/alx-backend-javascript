c20408d02eb93a4b920b8dc4cd631b2ba6f80b82
Solotole <solomontoleak47@gmail.com>
2024-08-12 16:41:54 +0100

    array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const object1 = { id: 1, firstName: 'Guillame', location: 'San Francisco' };
+  const object2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const object3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+
+  return [object1, object2, object3];
+}
