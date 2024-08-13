be9751314ee5bd7b4d6496fc2e3ec23cd0d790f0
Solotole <solomontoleak47@gmail.com>
2024-08-13 11:29:58 +0100

    set Traversal, join

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
   const newArray = [];
+  const sLength = startString.length;
   set.forEach((item) => {
-    if (item.substr(0, 3) === startString) {
-      newArray.push(item.slice(3, item.length));
+    if (item.substr(0, sLength) === startString && sLength !== 0) {
+      newArray.push(item.slice(sLength, item.length));
     }
   });
   const preferredArray = newArray.join('-');
