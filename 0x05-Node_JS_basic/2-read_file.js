51ec68e613fabe4a64293a2bb965cf0892e5a80c
Solotole <solomontoleak47@gmail.com>
2024-09-30 22:26:46 +0100

    reading a file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.trim().split('\n').filter(line => line.length > 0);
+    const lines = data.trim().split('\n').filter((line) => line.length > 0);
     if (lines.length === 0) throw new Error('Cannot load the database');
-    const header = lines.shift();
+    // const header = lines.shift();
     const students = {};
     let totalStudents = 0;
     lines.forEach((line) => {
       const studentData = line.split(',');
-      const [firstname, lastname, age, field] = studentData;
+      const [firstname, , , field] = studentData;
       if (!students[field]) {
         students[field] = [];
       }
