c1e932b2e9cdcb2fff2a784199c5aaa6e0981b99
Solotole <solomontoleak47@gmail.com>
2024-10-01 02:03:20 +0100

    complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
