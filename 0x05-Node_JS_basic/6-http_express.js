840191ab1e6009bdce72a62bc6361a7aca902189
Solotole <solomontoleak47@gmail.com>
2024-10-01 01:52:20 +0100

    simple Express HTTP server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 1245;
 
@@ -8,6 +9,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
 
 module.exports = app;
