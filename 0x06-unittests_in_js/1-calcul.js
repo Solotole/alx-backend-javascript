5d852bb898d46736cac1267c12149909d7c2220a
Solotole <solomontoleak47@gmail.com>
2024-10-03 16:22:01 +0100

    with type argument considered

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+  const first = Math.round(a);
+  const second = Math.round(b);
+  let result = 0;
+  if (type === "SUM") {
+    result = first + second;
+  } else if (type === "SUBTRACT") {
+    result = first - second;
+  } else if (type === "DIVIDE") {
+    if (second === 0) {
+      return "Error";
+    } else if (second !== 0) {
+      result = first / second;
+    }
+  }
+  return result;
+}
+
+module.exports = calculateNumber;
