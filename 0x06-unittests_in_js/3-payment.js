e3f5f6bbf05725ad7dadab6721b2c90339ce3ae2
Solotole <solomontoleak47@gmail.com>
2024-10-03 21:26:22 +0100

    sinon.spy a method

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,5 @@
+const Utilis = require('./utils')
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
