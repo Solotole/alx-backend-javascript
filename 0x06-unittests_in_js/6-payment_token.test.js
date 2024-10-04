361c91c42ee9c20c200ac313bce4a10205f8d74b
Solotole <solomontoleak47@gmail.com>
2024-10-04 16:29:10 +0100

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,18 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a successful response when success is true', (done) => {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    });
+  });
+
+  it('should return nothing when success is false', (done) => {
+    getPaymentTokenFromAPI(false).then((response) => {
+      expect(response).to.be.undefined;
+      done();
+    });
+  });
+});
