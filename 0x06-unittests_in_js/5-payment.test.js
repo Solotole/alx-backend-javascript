bf0191602612ee4c200749b66d31fbe5324ac083
Solotole <solomontoleak47@gmail.com>
2024-10-04 16:21:19 +0100

    Hooks on sinion

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" and call console.log once when inputs are 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+
+  it('should log "The total is: 20" and call console.log once when inputs are 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
